aa0e3daaa4c1dc35d047c4e050cf62feff07887c
Ian Cullum <cullumi@wwu.edu>
2024-06-13 05:00:32 -0700

    progress_report_test node reports on _process (effectively caching multiple progress reports).

--- scenes/demos/cellular/progress_report_test.gd
+++ scenes/demos/cellular/progress_report_test.gd
@@ -1,33 +1,42 @@
 @tool
 extends Node
 
-@export var report_increment:int = 1000
+@export var start_time:int
+@export var report_steps_immediately = false
+
 @export var report_name:String = ""
 @export var step_max:int = 0
 @export var step:int = 0
-@export var tried_once:bool = false
-@export var try_only_once:bool = false
-@export var start_time:int
+
+@export var prev_step:int = 0
+@export var prev_step_max:int = 0
+@export var prev_report_name:String
+
+func _process(_delta):
+	if not report_steps_immediately:
+		process_step()
+
+func process_step():
+	if step != prev_step:
+		print("Progress: (%d/%d)" % [step, step_max])
+		if (step == step_max):
+			var time_elapsed :int = Time.get_ticks_msec() - start_time
+			print("%s: Section took %s seconds" % [report_name,  (float(time_elapsed) / 1000)])
+		prev_step = step
 
 func _on_generation_finished():
 	print("Generation Finished")
 
 func _on_generation_progressed(steps:int):
-	step += steps
-	if (not try_only_once or not tried_once):
-		if (not (step % report_increment) or step == step_max):
-			tried_once = true
-			print("Progress: (%d/%d)" % [step, step_max])
-			if (step == step_max):
-				var time_elapsed :int = Time.get_ticks_msec() - start_time
-				print("%s: Section took %s seconds" % [report_name,  (float(time_elapsed) / 1000)])
+	step = steps
+	if report_steps_immediately or step >= step_max:
+		process_step()
 
 func _on_generation_section_started(step_count:int, section_name:String):
 	step_max = step_count
 	report_name = section_name
 	step = 0
 	start_time = Time.get_ticks_msec()
-	tried_once = false
 	print("Section: %s (0/%d)" % [section_name, step_count])
 
 func _on_generation_started():
